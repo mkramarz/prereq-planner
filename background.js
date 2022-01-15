f7985933d47bea73bcdf572dd6af1e938dc930c2
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-01-15 16:19:22 -0500

    more parsing

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-function get_reqs() {
-    let list = document.getElementsByClassName("catalog-notes")[0];
+function get_reqs(doc) {
+    let list = doc.getElementsByClassName("catalog-notes")[0];
     let bullets = list.getElementsByTagName("li");
     const reqs = [[],[]] //First array for prereqs, second array for coreqs
 
@@ -8,12 +8,19 @@ function get_reqs() {
         if (text.includes("Prerequisite:") || text.includes("Prerequisites:") || text.includes("Prerequisite(s):")) {
             parse_reqs(bullet.childNodes[0]);
         }
-        else if (text.includes("Coerequisite:") || text.includes("Prerequisites:") || text.includes("Prerequisite(s):") {
-
+        else if (text.includes("Coerequisite:") || text.includes("Prerequisites:") || text.includes("Prerequisite(s):")) {
+            parse_reqs(bullet.childNodes[0]);
         }
     }
 }
 
 function parse_reqs(node) {
-    links = node.getElementsByTagName('a');
+    let links = node.getElementsByTagName("a");
+    for (let link of links) {
+        parse_child(link.href)
+    }
+}
+
+function parse_child(url) {
+
 }
