6486900522459f8aaab6018df97f514c0f053b6e
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-01-15 20:57:39 -0500

    Began fixing background process

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,12 @@
-document.onload(() => {
-    window.postMessage("fetch")
+document.getElementById("sendMessage").addEventListener("click", () => {
+    window.postMessage("fetch");
+    console.log("Msg sent");
 })
 
 window.addEventListener("message", (event) => {
     if (event.source != window) {
         return;
     }
+    console.log(event.data);
     //TODO: What to do when recieving the courses back
 })
