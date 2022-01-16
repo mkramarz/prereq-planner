b90711309bb35e8c0a35c2ea4ec8c27fcd141c25
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-01-16 09:04:08 -0500

    Fixed indentation

--- popup/popup.js
+++ popup/popup.js
@@ -8,48 +8,48 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
 
 function populateList(course, parentList){
-  let item = document.createElement("li");
-  if(course.group!=-1)
-    item.innerHTML= course.name + "<sup>" +course.group + "</sup>";
-  else
-    item.innerText = course.name;
-  parentList.appendChild(item);
+	let item = document.createElement("li");
+	if(course.group!=-1)
+		item.innerHTML= course.name + "<sup>" +course.group + "</sup>";
+	else
+		item.innerText = course.name;
+	parentList.appendChild(item);
 
-  if (course.prereqs.length!=0){
-    let prereqs = course.prereqs; 
-    let subList = document.createElement("ul");
-    parentList.appendChild(subList);
+	if (course.prereqs.length!=0){
+		let prereqs = course.prereqs; 
+		let subList = document.createElement("ul");
+		parentList.appendChild(subList);
 
-    prereqs.forEach((item)=>{
-      if (item.prereqs.length!=0)
-        populateList(item, subList)
-      else
-      {
-        let subItem = document.createElement("li");
-        if(item.group!=-1)
-          subItem.innerHTML = item.name + "<sup>" + item.group + "</sup>";
-        else
-          subItem.innerText = item.name;
-        subList.appendChild(subItem);
-      }
-    })
+		prereqs.forEach((item)=>{
+			if (item.prereqs.length!=0)
+				populateList(item, subList)
+			else
+			{
+				let subItem = document.createElement("li");
+				if(item.group!=-1)
+					subItem.innerHTML = item.name + "<sup>" + item.group + "</sup>";
+				else
+					subItem.innerText = item.name;
+				subList.appendChild(subItem);
+			}
+		})
 
-    if (course.coreqs.length!=0){
-      let prereqs = course.coreqs; 
-  
-      prereqs.forEach((item)=>{
-        if (item.coreqs.length!=0)
-          populateList(item, subList)
-        else
-        {
-          let parentItem = document.createElement("li");
-          if(item.group!=-1)
-            parentItem.innerHTML = item.name + " [Corequisite]<sup>" + item.group + "</sup>";
-          else
-            parentItem.innerText = item.name +" [Corequisite]";
-          parentList.appendChild(parentItem);
-        }
-      })
-    }
-  }
+		if (course.coreqs.length!=0){
+			let prereqs = course.coreqs; 
+	
+			prereqs.forEach((item)=>{
+				if (item.coreqs.length!=0)
+					populateList(item, subList)
+				else
+				{
+					let parentItem = document.createElement("li");
+					if(item.group!=-1)
+						parentItem.innerHTML = item.name + " [Corequisite]<sup>" + item.group + "</sup>";
+					else
+						parentItem.innerText = item.name +" [Corequisite]";
+					parentList.appendChild(parentItem);
+				}
+			})
+		}
+	}
 }
