330e66b0710c411a5a1cf29bb3d6d0d605c45136
mkramarz <87330714+mkramarz@users.noreply.github.com>
2022-01-16 11:29:15 -0500

    Better style for background.js

--- background.js
+++ background.js
@@ -4,66 +4,45 @@ import('./course.js')
     let groupNum = 1;
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        // This handles listening for the extension's message to begin, and responding with the courses
         if (request.message === "fetch") {
-            groupNum = 1;
+            groupNum = 1; // Remember to reset group number every time the script runs!
             getReqs(document)
             .then((res) => {
-                console.log(res);
                 sendResponse(res);
             });
         }
-        return true;
+        return true; // Returning true here prevents the port from closing prematurely
     });
 
-    
-
     async function getReqs(doc, group=-1) {
+        // Reference variables
         let list = doc.getElementsByClassName("catalog-notes")[0];
         let bullets = list.getElementsByTagName("li");
         let code = doc.getElementById("page-title").innerText.trim().slice(0,8);
         let prereqs = [];
         let coreqs = [];
-        //console.log("Found " + code);
 
+        // For each bullet, check if its the prerequisite or corequisite bullet
         for (let bullet of bullets) {
             let text = bullet.childNodes[0].innerHTML
             if (text.includes("Prerequisite:") || text.includes("Prerequisites:") || text.includes("Prerequisite(s):")) {
-                prereqs = await logicParse(bullet.childNodes[0]);
+                prereqs = await logicParse(bullet.childNodes[0]); // Parse all links
             }
             else if (text.includes("Corequisite:") || text.includes("Corequisites:") || text.includes("Corequisite(s):")) {
-                coreqs = await logicParse(bullet.childNodes[0]);
+                coreqs = await logicParse(bullet.childNodes[0]); // Same here
             }
         }
-        return new Module.Course(code, prereqs, coreqs, group);
-    }
-
-    async function parseReqs(node) {
-        let links = node.getElementsByTagName("a");
-        let reqs = []
-        if (links.length == 0) {
-            //console.log("No links to follow");
-        }
-        for (let link of links) {
-            //console.log("Following link to " + link.innerText);
-            reqs.push(await parseChild(link.href));
-        }
-        return reqs
-    }
-
-    async function parseChild(url, group=-1) {
-        let result = await fetch(url);
-        let text = await result.text()
-        const doc = new DOMParser().parseFromString(text, 'text/html');
-        let reqResult = getReqs(doc, group);
-        return reqResult;
+        return new Module.Course(code, prereqs, coreqs, group); // Finally, return a course with the data
     }
 
     async function logicParse(node) {
         let links = node.getElementsByTagName("a");
         let text = node.innerHTML;
         let reqs = []
+
+        //Handle our base cases first
         if (links.length == 0 || typeof node == "undefined") {
-            //console.log("No links to follow");
             return [];
         }
 
@@ -84,8 +63,9 @@ import('./course.js')
         text = text.replace(/([^0-9,\/])/g, "");
         text = text + "."
 
-        console.log(text);
-
+        //Now, iterate through the sequence we created
+        //If an alias is followed by a slash (disjunction), we enable grouping
+        //Then, we disable grouping if we come across a comma (conjunction)
         let grouping = -1;
         for (let i = 0; i < text.length-1; i++) {
             let char = text[i];
@@ -95,14 +75,25 @@ import('./course.js')
                 }
             }
             else { //If we are on a link alias
-                if (grouping == -1 && text[i+1] == '/') {
+                if (grouping == -1 && text[i+1] == '/') { //Disjunction operator
                     grouping = groupNum;
                     groupNum++;
                 }
-                console.log(links[parseInt(text[i])].href + " in group " + grouping);
+                // Add the corresponding link in our listing
                 reqs.push(await parseChild(links[parseInt(text[i])].href, grouping));
             }
         }
         return reqs;
+    }
+
+    async function parseChild(url, group=-1) {
+        // Fetching is async, which is why every other function is async too
+        let result = await fetch(url);
+        let text = await result.text()
+        // Turn our fetched HTML page into a new DOM
+        const doc = new DOMParser().parseFromString(text, 'text/html');
+        // Then recursively parse that new DOM
+        let reqResult = getReqs(doc, group);
+        return reqResult;
     }
 });
